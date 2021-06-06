fa23889ed237bc0d28e0e931bd251515aabfbaf3
Manojit Chakraborty <jeetmanojit98@gmail.com>
2021-06-06 10:38:53 +0530

    initial commit

---
+++ Reverse.cpp
@@ -0,0 +1,20 @@
+//Reverse no.
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    system("cls");
+    int n;
+    cin>>n;
+    int rev=0;
+    while (n>0)
+    {
+        int last=n%10;
+        rev=rev*10 + last;
+        n=n/10;
+    }
+    cout<<rev;
+   
+}
+
